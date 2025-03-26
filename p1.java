04fae8fe070df5568ba07fa4be21583ff0f5df5e
saikalekar2006 <saikalekar007@gmail.com>
2025-03-26 10:44:00 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,9 +1,7 @@
 public class p1 {
-	public static void main(String[] args);
-	{
+	public static void main(String[] args){
 		//TODO Auto-generated method stub
-		System.out.println("Hello Sai");
-			System.out.println("");
+		System.out.println("Hello Sai");	
 	}
 
 }
