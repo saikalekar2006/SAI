87d49937dfec874d2a46f6a89e16a2e428e16640
Manthan033 <manthanrokhade03@gmail.com>
2025-03-29 11:07:14 +0530

    change strings

--- p1.java
+++ p1.java
@@ -1,8 +1,6 @@
 public class p1 {
 	public static void main(String[] args){
 		//TODO Auto-generated method stub
-		System.out.println("Hello Sai");
+		System.out.println("Hello World");
 	}
-	System.out.println(");
-System.out.println();
 }
